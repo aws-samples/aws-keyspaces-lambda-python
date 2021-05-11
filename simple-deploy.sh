812f735388b01e63d40ac3d306713ba8780515cf
deepaksinghcs14 <Deepak.singh@mailsphere.co.uk>
2021-05-11 23:15:25 +0530

    fix for the issue https://github.com/aws-samples/aws-keyspaces-lambda-python/issues/1

--- simple-deploy.sh
+++ simple-deploy.sh
@@ -1,4 +1,4 @@
-npm install cdk@1.33.0 -y
+npm install cdk@1.0.0 -y #1.33.0 version was not working
 python3 -m venv venv/
 source venv/bin/activate
 cwd=$(pwd)
@@ -10,9 +10,11 @@ cd $cwd/lambda/.dist
 rm lambda.zip || true
 zip -r9 lambda.zip * -x "bin/*" "pip*" "pylint*" "setuptools*"
 cd $cwd/lambda/
+cp $cwd/lambda/../AmazonRootCA1.pem $cwd/lambda/AmazonRootCA1.pem #this file needs to be sent
 zip -r9 .dist/lambda.zip * -x ".dist"
 cd $cwd
 npx cdk synth
+npx cdk bootstrap aws://1234567/ap-south-1 #we need this line as well to work thing out
 npx cdk deploy $*
 cd infrastructure
 ./set_secrets.sh $*
